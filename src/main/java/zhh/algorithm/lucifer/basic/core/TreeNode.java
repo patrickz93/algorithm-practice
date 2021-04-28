7921aa0e386d7c23d6cb68aad2d3e149be6f37db
zhanghao <zhanghao@xiaobangtouzi.com>
2021-04-29 07:44:23 +0800

    循环遍历二叉树-stack

---
+++ src/main/java/zhh/algorithm/lucifer/basic/core/TreeNode.java
@@ -0,0 +1,24 @@
+package zhh.algorithm.lucifer.basic.core;
+
+
+/**
+ * @author zhanghao
+ * @date 2021-04-28 07:32
+ * @desc
+ */
+public class TreeNode {
+    public int val;
+    /**
+     * 左子树
+     */
+    public TreeNode left;
+
+    /**
+     * 右子树
+     */
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
