77012c4d0fa512ea36860375c30e70364046b367
zhanghao <zhanghao@xiaobangtouzi.com>
2021-05-28 07:36:10 +0800

    11. 盛最多水的容器

--- src/main/java/zhh/algorithm/lucifer/basic/day1/array/DoublePointer.java
+++ src/main/java/zhh/algorithm/lucifer/basic/day1/array/DoublePointer.java
@@ -46,9 +46,67 @@ public class DoublePointer {
         return ans;
     }
 
+    /**
+     * 11. 盛最多水的容器
+     * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0) 。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+     * 说明：你不能倾斜容器。
+     *
+     * 示例 1：
+     *
+     * 输入：[1,8,6,2,5,4,8,3,7]
+     * 输出：49
+     * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+     *
+     *  示例 2：
+     * 输入：height = [1,1]
+     * 输出：1
+     *
+     * 示例 3：
+     * 输入：height = [4,3,2,1,4]
+     * 输出：16
+     *
+     *  示例 4：
+     * 输入：height = [1,2,1]
+     * 输出：2
+     *
+     * 提示：
+     * n = height.length
+     * 2 <= n <= 3 * 104
+     * 0 <= height[i] <= 3 * 104
+     *
+     * 链接：https://leetcode-cn.com/problems/container-with-most-water
+     *
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length-1;
+        int maxArea = 0;
+        while (right > left) {
+            maxArea = Math.max(maxArea, (right - left) * Math.min(height[left], height[right]));
+            if (height[left] > height[right]) {
+                --right;
+            } else {
+                ++left;
+            }
+        }
+        return maxArea;
+    }
+
     public static void main(String[] args) {
         DoublePointer doublePointer = new DoublePointer();
         int[] trapInput = {0,1,0,2,1,0,1,3,2,1,2,1};
         Assert.assertEquals(6,doublePointer.trapRainWater(trapInput));
+
+        int[] maxAreaInput1 = {1,8,6,2,5,4,8,3,7};
+        Assert.assertEquals(49, doublePointer.maxArea(maxAreaInput1));
+        int[] maxAreaInput2 = {1,1};
+        Assert.assertEquals(1, doublePointer.maxArea(maxAreaInput2));
+        int[] maxAreaInput3 = {4,3,2,1,4};
+        Assert.assertEquals(16, doublePointer.maxArea(maxAreaInput3));
+        int[] maxAreaInput4 = {1,2,1};
+        Assert.assertEquals(2, doublePointer.maxArea(maxAreaInput4));
     }
 }
